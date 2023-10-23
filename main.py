de21bbea15b7ec0ad22488a7e3a3bbc68bdd7ad3
Nikolay Andreev <bakasaru@list.ru>
2023-10-23 18:50:02 +0300

    feat: add simple Telegram bot

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-import requests
-import tabulate
 import telebot
-import json
+import os
+from dotenv import load_dotenv
 
-with open('data.json') as file:
-    data = json.load(file)
+load_dotenv()
 
-print(data['marks'])
+TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
 
-data['age'] = 87
+bot = telebot.TeleBot(TELEGRAM_TOKEN, parse_mode=None)
 
-with open('result.json', 'w') as result:
-    json.dump(data, result, indent=2)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    markup = telebot.types.ReplyKeyboardMarkup(row_width=2)
+    button_1 = telebot.types.KeyboardButton('Hello')
+    button_2 = telebot.types.KeyboardButton('from')
+    button_3 = telebot.types.KeyboardButton('AIP')
+    markup.add(button_1, button_2, button_3)
+
+    bot.send_message(message.chat.id, message.text, reply_markup=markup)
+
+
+bot.infinity_polling()
